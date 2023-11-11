6ef921b39c40d702cacc34b05453f2c4b8b11539
Agmund2002 <malysh.andrey2000@gmail.com>
2023-11-11 17:32:00 +0200

    added gh-page and removed everything about the redux-persist

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from 'redux/store';
 
 const theme = {
   colors: {
@@ -22,10 +21,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
           <App />
           <GlobalStyle />
-        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
